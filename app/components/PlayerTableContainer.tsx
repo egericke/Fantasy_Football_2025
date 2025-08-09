3dc232251589df7c03121b80e1a84ed655859861
egericke <70616620+egericke@users.noreply.github.com>
2025-08-09 12:37:58 -0400

    Update PlayerTableContainer.tsx

--- app/components/PlayerTableContainer.tsx
+++ app/components/PlayerTableContainer.tsx
@@ -1,284 +1,110 @@
-import * as React from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import { connect } from 'react-redux';
-import { IPlayer, Player } from '../lib/models/Player';
+import { IPlayer } from '../lib/models/Player';
+import { IPlayers } from '../lib/models/Players';
+import { ITeam } from '../lib/models/Team';
 import { IScoring } from '../lib/models/Scoring';
-import { NullablePlayer } from '../lib/models/Team';
-import { onRemovePlayer } from '../lib/store/actions/players';
-import { onPickPlayer, skipPick, undoLast } from '../lib/store/actions/teams';
-import { IStoreState } from '../lib/store/store';
 import PlayerTable from './PlayerTable';
+import * as playerActions from '../lib/store/actions/players';
+import * as teamActions from '../lib/store/actions/teams';
 
-interface IPlayerTableProps {
-  /**
-   * Key is bye week number, value is whether there's a conflict with starter
-   */
-  byeWeeks: { [key: number]: boolean };
-  currentPick: number;
-  mobile?: boolean;
-  undraftedPlayers: any[];
-  onPickPlayer: (player: IPlayer) => void;
-
-  /**
-   * Key is the team name, defined for a team if there's a player on
-   * the drafters roster that's an RB from that team
-   */
-  rbHandcuffTeams: { [key: string]: boolean };
-  onRemovePlayer: (player: IPlayer) => void;
-
+interface IPlayerTableContainerProps {
+  players: IPlayers;
+  teams: ITeam[];
   scoring: IScoring;
-
-  skip: () => void;
-  undo: () => void;
-
-  /**
-   * Array of positions (key) and whether the drafter has NOT filled those positions.
-   * True if it's still not filled (ie valuable)
-   */
-  valuedPositions: { [key: string]: boolean };
+  addPlayer: (player: IPlayer) => void;
+  removePlayer: (player: IPlayer) => void;
+  movePlayer: (player: IPlayer, newIndex: number) => void;
+  updateTeam: (team: ITeam) => void;
 }
 
-interface IPlayerTableState {
-  /**
-   * The name of the player the user is searching for.
-   * Only show players with a similar name
-   */
-  nameFilter: string;
+const PlayerTableContainer: React.FC<IPlayerTableContainerProps> = ({
+  players,
+  teams,
+  scoring,
+  addPlayer,
+  removePlayer,
+  movePlayer,
+  updateTeam,
+}) => {
+  const { QB, RB, WR, TE, FLEX, K, DST } = teams.reduce(
+    (acc, t) => {
+      const { QB, RB, WR, TE, FLEX, K, DST } = t;
+      return {
+        QB: [...acc.QB, ...QB],
+        RB: [...acc.RB, ...RB],
+        WR: [...acc.WR, ...WR],
+        TE: [...acc.TE, ...TE],
+        FLEX: [...acc.FLEX, ...FLEX],
+        K: [...acc.K, ...K],
+        DST: [...acc.DST, ...DST],
+      };
+    },
+    { QB: [], RB: [], WR: [], TE: [], FLEX: [], K: [], DST: [] },
+  );
 
-  /**
-   * Only show players in these positions, hide the rest
-   */
-  /**
-   * Positions currently selected to filter the player list.  Using
-   * `Player['Pos']` restricts this list to actual player positions (QB, RB,
-   * WR, TE, DST, K), while allowing the special "?" token to represent
-   * “all positions”.  Without constraining the type here, the state may
-   * contain other values like FLEX or BENCH which are not valid in the
-   * PlayerTable and cause type errors during compilation.
-   */
-  positionsToShow: (Player['Pos'] | '?')[];
-}
+  const byeWeeks = [...QB, ...RB, ...WR, ...TE, ...FLEX].reduce(
+    (acc, p) => ({ ...acc, [p.Team]: p.byeWeek }),
+    {},
+  );
 
-/**
- * A table displaying all the undrafted players
- *
- * Includes buttons for skipping the current round, without a pick,
- * and undoing the last round/pick (in the event of a mistake)
- */
-class PlayerTableContainer extends React.PureComponent<IPlayerTableProps, IPlayerTableState> {
-  public static defaultProps = {
-    mobile: false,
-  };
-
-  public state: IPlayerTableState = {
-    nameFilter: '',
-    positionsToShow: ['?'], // ? is a hackish flag for "ALL"
-  };
-
-  /** clear name filter after a player was picked */
-  public componentDidUpdate = ({}, { nameFilter }: IPlayerTableState) => {
-    if (nameFilter === this.state.nameFilter) {
-      this.setState({ nameFilter: '' });
-    }
-  };
-
-  public render() {
-    const { currentPick, mobile, rbHandcuffTeams, scoring, undraftedPlayers: players, valuedPositions } = this.props;
-    const { nameFilter, positionsToShow } = this.state;
-
-    // players after filtering by position
-    // array of booleans for whether not to show the player
-    let filteredPlayers =
-      positionsToShow.length === 1 && positionsToShow[0] === '?'
-        ? new Array(players.length).fill(false)
-        : players.map((p) => positionsToShow.indexOf(p.pos) < 0);
-
-    // filter by the nameFilter (for name and team)
-    const nameFilterLower = nameFilter.toLowerCase();
-    if (nameFilterLower) {
-      filteredPlayers = filteredPlayers.map((filtered, i) => {
-        if (filtered) {
-          // it's already being filtered out
-          return true;
-        }
-
-        // check for whether the name, split is similar to nameFilter
-        const { name } = players[i];
-        const lowercaseName = name.toLowerCase();
-        const names = lowercaseName.split(' ');
-        const firstNameMatch = names[0].startsWith(nameFilterLower);
-        const lastNameMatch = names[1] ? names[1].startsWith(nameFilterLower) : false;
-
-        // filter out all players but those that match the search term
-        return !(lowercaseName.startsWith(nameFilterLower) || firstNameMatch || lastNameMatch);
-      });
-    }
-
-    const adpDiff = [0, 0.5, 1].map((ppr) => Math.abs(ppr - scoring.receptions));
-    const minDiff = Math.min(...adpDiff);
-    const minDiffIndex = adpDiff.indexOf(minDiff);
-    const adpCol = { 0: 'std', 1: 'halfPpr', 2: 'ppr' }[minDiffIndex]!;
-
-    // players that will be drafted soon
-    const draftSoon = players.map((p, i) => !filteredPlayers[i] && p[adpCol] > 0 && currentPick + 8 >= p[adpCol]);
-
-    // players that are RB handcuffs
-    const rbHandcuffs = new Set(
-      players.filter((p, i) => !filteredPlayers[i] && p.pos === 'RB' && rbHandcuffTeams[p.team])
-    );
-
-    // players that will be recommended
-    let recommendedCount = 0;
-    const recommended = players.reduce((acc, p, i) => {
-      // for first few, it's likely a yes
-      if (recommendedCount < 3 && valuedPositions[p.pos]) {
-        if (draftSoon[i] || p[adpCol] === 0) {
-          // accounting for players w/ a lack of adps
-          recommendedCount += 1;
-          return [...acc, p];
-        }
-      } else if (i < 30 && rbHandcuffs.has(p)) {
-        recommendedCount += 1;
-        return [...acc, p];
-      }
-      return acc;
-    }, []);
-
-    return (
-      <PlayerTable
-        {...this.props}
-        players={players}
-        draftSoon={draftSoon}
-        filteredPlayers={filteredPlayers}
-        nameFilter={nameFilter}
-        mobile={!!mobile}
-        recommended={new Set(recommended)}
-        positionsToShow={positionsToShow}
-        rbHandcuffs={rbHandcuffs}
-        setNameFilter={this.setNameFilter}
-        togglePositionFilter={this.togglePositionFilter}
-        resetPositionFilter={this.resetPositionFilter}
-      />
-    );
-  }
-
-  /**
-   * update the allowable positions in state, used to filter out players by position
-   */
-  private togglePositionFilter = (position: Player['Pos'] | '?') => {
-    let { positionsToShow } = this.state;
-
-    // if it's ?, clear anything else
-    if (position === '?') {
-      this.setState({ positionsToShow: ['?'] });
-    } else if (positionsToShow.indexOf(position) > -1) {
-      positionsToShow = positionsToShow.filter((p) => p !== position);
-      this.setState({
-        positionsToShow: positionsToShow.length ? positionsToShow : ['?'],
-      });
-    } else {
-      positionsToShow = positionsToShow.filter((p) => p !== '?');
-      this.setState({
-        positionsToShow: positionsToShow.concat([position]),
-      });
-    }
-  };
-
-  /**
-   * reset the position filter so all positions are shown. Called after a player is selected.
-   */
-  private resetPositionFilter = () => {
-    this.setState({ positionsToShow: ['?'] });
-  };
-
-  /**
-   * update the filter for searching for a plauer by name or team
-   */
-  private setNameFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ nameFilter: e.target.value });
-  };
-}
-
-const mapStateToProps = (state: IStoreState) => {
-  const { QB, RB, WR, TE, FLEX, SUPERFLEX, DST, K, BENCH } = state.teams[state.trackedTeam];
-
-  // add the positions to the object that the trackedTeam hasn't
-  // filled their roster with (ie they have space for)
-  const notFilled = (pos: NullablePlayer[]) => !pos.every((p: IPlayer) => !!p);
-  const valuedPositions = {} as any;
-  if (notFilled(QB)) {
-    valuedPositions.QB = true;
-  }
-  if (notFilled(RB)) {
-    valuedPositions.RB = true;
-  }
-  if (notFilled(WR)) {
-    valuedPositions.WR = true;
-  }
-  if (notFilled(FLEX)) {
-    valuedPositions.RB = true;
-    valuedPositions.WR = true;
-    valuedPositions.TE = true;
-  }
-  if (notFilled(SUPERFLEX)) {
-    valuedPositions.QB = true;
-    valuedPositions.RB = true;
-    valuedPositions.WR = true;
-    valuedPositions.TE = true;
-  }
-  if (notFilled(TE)) {
-    valuedPositions.TE = true;
-  }
-
-  // after one of each main starter has been drafted, everything is valued
-  if (!Object.keys(valuedPositions).length) {
-    // always want more RBs and WRs
-    valuedPositions.RB = true;
-    valuedPositions.WR = true;
-
-    // only want one backup QB and TE
-    const qbBackupCount = BENCH.filter((p) => p && p.pos === 'QB').length;
-    if (qbBackupCount < 1) {
-      valuedPositions.QB = true;
-    }
-    const teBackupCount = BENCH.filter((p) => p && p.pos === 'TE').length;
-    if (teBackupCount < 1) {
-      valuedPositions.TE = true;
-    }
-  }
-
-  // only want one of each K and DST, none on bench
-  if (notFilled(K)) {
-    valuedPositions.K = true;
-  }
-  if (notFilled(DST)) {
-    valuedPositions.DST = true;
-  }
-
-  // find the bye weeks already taken by the core players (QB, RB, WR, FLEX)
-  const byeWeeks = [...QB, ...RB, ...WR, ...FLEX, ...SUPERFLEX]
-    .map((p) => p && p.bye)
-    .reduce((acc, bye) => (bye ? { ...acc, [bye]: true } : acc), {});
-
-  // find the teams of the rbs, other rbs on these teams will be handcuffs
   const rbHandcuffTeams = [...RB, ...FLEX]
     .filter((p: IPlayer) => p && p.pos === 'RB')
-    .reduce((acc, p: IPlayer) => ({ ...acc, [p.team]: true }), {});
+    .reduce((acc, p: IPlayer) => ({ ...acc, [p.Team]: true }), {});
 
   return {
     byeWeeks,
-    currentPick: state.currentPick,
     rbHandcuffTeams,
-    scoring: state.scoring,
-    undraftedPlayers: state.undraftedPlayers,
-    valuedPositions,
+    addPlayer,
+    removePlayer,
+    movePlayer,
+    updateTeam,
+    teams,
+    scoring,
+    players: {
+      ...players,
+      FLEX: players.FLEX.map((p) => {
+        if (p.pos === 'RB') {
+          return {
+            ...p,
+            isHandcuff: !!rbHandcuffTeams[p.Team],
+          };
+        }
+        return p;
+      }),
+      RB: players.RB.map((p) => ({
+        ...p,
+        isHandcuff: !!rbHandcuffTeams[p.Team],
+      })),
+    },
   };
 };
 
-const mapDispatchToProps = (dispatch: any) => ({
-  onPickPlayer: (player: IPlayer) => dispatch(onPickPlayer(player)),
-  onRemovePlayer: (player: IPlayer) => dispatch(onRemovePlayer(player)),
-  skip: () => dispatch(skipPick()),
-  undo: () => dispatch(undoLast()),
+const mapStateToProps = (state) => ({
+  players: state.players,
+  teams: state.teams,
+  scoring: state.scoring,
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(PlayerTableContainer);
+const mapDispatchToProps = {
+  addPlayer: playerActions.addPlayer,
+  removePlayer: playerActions.removePlayer,
+  movePlayer: playerActions.movePlayer,
+  updateTeam: teamActions.updateTeam,
+};
+
+const ConnectedPlayerTableContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PlayerTable);
+
+const PlayerTableWrapper: React.FC = () => {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <ConnectedPlayerTableContainer />
+    </DndProvider>
+  );
+};
+
+export default PlayerTableWrapper;
