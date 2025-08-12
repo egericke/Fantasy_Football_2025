073dda5d21afd7d9008be20f7dd371eb5b7d5ee6
egericke <70616620+egericke@users.noreply.github.com>
2025-08-12 12:18:01 -0400

    Update PickHistoryContainer.tsx

--- app/components/PickHistoryContainer.tsx
+++ app/components/PickHistoryContainer.tsx
@@ -1,100 +1,52 @@
 import * as React from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import { connect } from 'react-redux';
-import { IPick, ITeam } from '../lib/models/Team';
-import { setTrackedTeam, undoPick } from '../lib/store/actions/teams';
+import { IPlayer } from '../lib/models/Player';
+import { ITeam } from '../lib/models/Team';
 import { IStoreState } from '../lib/store/store';
-import PickHistory from './PickHistory';
+import PlayerTable from './PlayerTable';
+import * as playerActions from '../lib/store/actions/players';
+import * as teamActions from '../lib/store/actions/teams';
 
-interface IPickHistoryContainerProps {
-  activeTeam: number;
-  currentPick: number;
-  numberOfTeams: number;
-  picks: IPick[];
-  setTrackedTeam: (team: number) => void;
-  teams: ITeam[];
-  trackedTeam: number;
-  undoPick: (pick: IPick) => void;
-}
-
-interface IPickHistoryContainerState {
-  cardLength: number;
-  open: boolean;
-}
-
-class PickHistoryContainer extends React.Component<IPickHistoryContainerProps, IPickHistoryContainerState> {
-  public pickRowRef: any = React.createRef();
-
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      cardLength: 45,
-      open: true,
-    };
-  }
-
-  public componentDidMount = () => {
-    this.setState({
-      cardLength: this.getCardLength(this.props.numberOfTeams),
-    });
-
-    window.addEventListener('resize', () =>
-      this.setState({
-        cardLength: this.getCardLength(this.props.numberOfTeams),
-      })
-    );
-  };
-
-  public componentDidUpdate = () => {
-    // scroll the pick row back to the left
-    if (this.pickRowRef.current) {
-      this.pickRowRef.current.scrollLeft = 0;
-    }
-  };
-
-  public toggleOpen = () => {
-    this.setState({ open: !this.state.open });
-  };
-
-  private getCardLength = (numberOfTeams: number): number => {
-    if (typeof window === 'undefined') {
-      return 45;
-    }
-    const thisWidth = window.innerWidth * 0.65; // ~30px padding, 70% width of total window size
-    const cardLength = Math.floor(thisWidth / numberOfTeams); // 8 == 2px border, 6px margin
-    return Math.min(cardLength, 75);
-  };
-
-  public render() {
-    const { currentPick, numberOfTeams } = this.props;
-
-    // round tracker message
-    const headerMessage = `Round ${Math.ceil((currentPick + 1) / numberOfTeams)} – Pick ${currentPick + 1}`;
-
-    return (
-      <PickHistory
-        {...this.props}
-        {...this.state}
-        refProp={this.pickRowRef}
-        headerMessage={headerMessage}
-        toggleOpen={this.toggleOpen}
-      />
-    );
-  }
-}
-
-const mapStateToProps = ({ activeTeam, currentPick, numberOfTeams, picks, teams, trackedTeam }: IStoreState) => ({
-  activeTeam,
-  currentPick,
-  numberOfTeams,
-  picks,
-  teams,
-  trackedTeam,
+const mapStateToProps = (state: IStoreState) => ({
+  players: {
+    QB: state.undraftedPlayers.filter(p => p.pos === 'QB'),
+    RB: state.undraftedPlayers.filter(p => p.pos === 'RB'),
+    WR: state.undraftedPlayers.filter(p => p.pos === 'WR'),
+    TE: state.undraftedPlayers.filter(p => p.pos === 'TE'),
+    FLEX: state.undraftedPlayers.filter(p => ['RB', 'WR', 'TE'].includes(p.pos || '')),
+    K: state.undraftedPlayers.filter(p => p.pos === 'K'),
+    DST: state.undraftedPlayers.filter(p => p.pos === 'DST'),
+  },
+  teams: state.teams,
+  scoring: state.scoring,
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
-  setTrackedTeam: (teamToTrack: number) => dispatch(setTrackedTeam(teamToTrack)),
-  undoPick: (pick: IPick) => dispatch(undoPick(pick)),
+  addPlayer: (player: IPlayer) => dispatch(teamActions.onPickPlayer(player)),
+  removePlayer: (player: IPlayer) => dispatch(playerActions.onRemovePlayer(player)),
+  movePlayer: (player: IPlayer, newIndex: number) => {
+    // This would need a proper action if drag-and-drop reordering is implemented
+    console.log('Move player not implemented:', player, newIndex);
+  },
+  updateTeam: (team: ITeam) => {
+    // This would need a proper action for team updates
+    console.log('Update team not implemented:', team);
+  },
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(PickHistoryContainer);
+const ConnectedPlayerTableContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PlayerTable);
+
+const PlayerTableWrapper: React.FC<{ mobile?: boolean }> = ({ mobile }) => {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <ConnectedPlayerTableContainer mobile={mobile} />
+    </DndProvider>
+  );
+};
+
+export default PlayerTableWrapper;
